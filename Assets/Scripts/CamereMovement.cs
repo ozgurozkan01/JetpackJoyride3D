dba379e23c44263dd206ef52691350b76e3c0b96
Özgür ÖZKAN <2020555050@ogr.cu.edu.tr>
2022-08-20 02:09:30 +0300

    add player and camera movement

---
+++ Assets/Scripts/CamereMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CamereMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    void LateUpdate()
+    {
+        CameraFollowPlayer();
+    }
+
+    private void CameraFollowPlayer()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, player.transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    [SerializeField] private float jetPackSpeed;
+    [SerializeField] private float speed;
+    
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        MovePlayer();
+        if (Input.GetMouseButton(0))
+        {
+            JetPackActivate();
+        }
+    }
+    
+    private void MovePlayer()
+    {
+        _rigidbody.velocity = new Vector3(0f, _rigidbody.velocity.y, speed * Time.deltaTime);
+    }
+
+    private void JetPackActivate()
+    {
+        _rigidbody.velocity = new Vector3(0f, jetPackSpeed * Time.deltaTime, _rigidbody.velocity.z);
+    }
+}
